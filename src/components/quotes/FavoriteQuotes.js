f0c4c64c93abe3374e15741cd502b2e9a4f9b04b
lee_c <gitleelee@gmail.com>
2023-12-21 20:36:36 -0800

    Added finishing touches

--- src/components/quotes/FavoriteQuotes.js
+++ src/components/quotes/FavoriteQuotes.js
@@ -1,21 +1,37 @@
 import React from "react";
 import FavoriteQuoteCard from "./FavoriteQuoteCard";
 
-const favoriteQuotes = ({ favoriteQuotes, maxFaves, removeFromFavorites }) => {
-    return (
-        <section className="favorite-quotes">
-            <div className="wrapper quotes">
-                <h3>Top 3 favorite Quotes</h3>
-                {favoriteQuotes.length > 0 && (
+function FavoriteQuotes({ favoriteQuotes, maxFaves, removeFromFavorites }) {
+  const remainingFavoriteAmount = maxFaves - favoriteQuotes.length;
+  return (
+    <section className='favorite-quotes'>
+      <div className='wrapper quotes'>
+        <h3>Top 3 favorite quotes</h3>
+        {favoriteQuotes.length > 0 && (
           <ul>
-            {favoriteQuotes.map((quote) => (
-              <FavoriteQuoteCard key={quote.id} quote={quote} removeFromFavorites={removeFromFavorites} />
+            {favoriteQuotes.map((quote, index) => (
+              <FavoriteQuoteCard
+                key={quote.id}
+                listPosition={index + 1}
+                quote={quote}
+                removeFromFavorites={removeFromFavorites}
+              />
             ))}
           </ul>
         )}
-            </div>
-        </section>
-    )
+        {favoriteQuotes.length < maxFaves && (
+          <div className='favorite-quotes-description'>
+            <p>
+              You can add {remainingFavoriteAmount} more {remainingFavoriteAmount === 1 ? "quote" : "quotes"} to your top three
+              favorites by selecting from the options below.
+              <br />
+              Once you choose, they will appear here.
+            </p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
 
-export default favoriteQuotes;
+export default FavoriteQuotes;
