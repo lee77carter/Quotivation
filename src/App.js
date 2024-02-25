0412df1c35d71b10d7e2477cd57c3f2a15b30645
lee_c <gitleelee@gmail.com>
2024-02-25 00:09:34 -0800

    Add comments.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Import necessary React hooks and components
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -7,7 +8,9 @@ import Quotes from "./components/quotes/Quotes";
 import Message from "./components/Message";
 import "./App.css";
 
+// Main App component definition
 function App() {
+  // State variables using useState hook
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
@@ -20,6 +23,7 @@ function App() {
 
   const maxFaves = 3;
 
+  // Function to fetch quotes data from an external API
   const fetchQuotes = async () => {
     try {
       setLoading(true);
@@ -32,20 +36,25 @@ function App() {
     setLoading(false);
   };
 
+  // useEffect hook to fetch quotes data on component mount
   useEffect(() => {
     fetchQuotes();
   }, []);
 
+  // useEffect hook to save favoriteQuotes data to local storage when it changes
   useEffect(() => {
     window.localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes));
   }, [favoriteQuotes]);
 
+  // Filter quotes based on selected category
   const filteredQuotes = category !== "All" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
 
+  // Event handler for changing the selected category
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
   };
 
+  // Function to add a quote to favorites
   const addToFavorites = (quoteId) => {
     const selectedQuote = quotes.find((quote) => quote.id === quoteId);
     const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === selectedQuote.id);
@@ -64,22 +73,27 @@ function App() {
     }
   };
 
+  // Function to remove a quote from favorites
   const removeFromFavorites = (quoteId) => {
     const updatedFavorites = favoriteQuotes.filter((quote) => quote.id !== quoteId);
     setFavoriteQuotes(updatedFavorites);
   };
 
+  // Function to remove the message displayed to the user
   const removeMessage = () => {
     setShowMessage(false);
   };
 
+  // JSX structure for rendering the App component
   return (
     <div className='App'>
       {showMessage && <Message messageText={messageText} removeMessage={removeMessage} />}
       <Header />
       <main>
+        {/* Render the FavoriteQuotes component */}
         <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites} />
 
+        {/* Conditionally render loader or Quotes component based on loading state */}
         {loading ? (
           <Loader />
         ) : (
@@ -97,4 +111,6 @@ function App() {
     </div>
   );
 }
+
+// Export the App component as the default export
 export default App;
